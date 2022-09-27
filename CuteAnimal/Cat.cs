fd7aefe7064ef5a925f438ffae54d9777418b30e
andre-pedro <andrewgoesgaming@gmail.com>
2022-09-27 12:21:27 +0100

    Instantiate 3 cats, add lambda to GetName()

--- CuteAnimal/Cat.cs
+++ CuteAnimal/Cat.cs
@@ -10,13 +10,28 @@ namespace CuteAnimal
         private string name;
         private Mood mood;
         private Feed feed;
+        private int energy;
+        private Random rng;
         
-        public Cat(string name, Mood mood, Feed feed) 
+        public Cat(string name, Mood mood, Feed feed) : this()
         {
             this.name = name;
             this.mood = mood;
             this.feed = feed;
         }
+        public Cat(string name) : this(name, Mood.Happy, Feed.AboutToExplode) 
+        {
+
+        }
+
+        private Cat() 
+        {
+            rng = new Random();
+            energy = rng.Next(101);
+        }
+
+        public string GetName() => name;
+
 
     }
 }
--- CuteAnimal/Program.cs
+++ CuteAnimal/Program.cs
@@ -4,12 +4,18 @@ namespace CuteAnimal
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             Console.WriteLine("Hello, World!");
 
             Cat c1 = new Cat("Randall",Mood.Grumpy,Feed.Starving);
             Cat c2 = new Cat("Meowth",Mood.Hyperactive,Feed.Satisfied);
+            Cat c3 = new Cat("Timon");
+
+            Console.WriteLine(c1.GetName() + c2.GetName() + c3.GetName());
+            Console.WriteLine(c1.GetName());
+            Console.WriteLine(c2.GetName());
+            Console.WriteLine(c3.GetName());
         }
     }
 }
